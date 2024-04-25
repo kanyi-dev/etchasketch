79442e89e3d80af6b0524b807fb438fa2ac55b2a
nickanyi <130154999+kanyi-dev@users.noreply.github.com>
2024-04-25 14:54:13 +0300

    createBoard function to set grid size

--- main.js
+++ main.js
@@ -1,35 +1,10 @@
-const container = document.getElementById("grid-container");
-const gridSize = 16;
+document.addEventListener("DOMContentLoaded", function(){
+ createBoard(16);
+})
 
-function createGrid() {
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
-  }
+const createBoard = (size) => {
+  let board = document.querySelector(".board");
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
-
-function clearGrid() {
-  container.innerHTML = "";
-}
-
-function createNewGrid() {
-  const userInput = prompt(
-    "Enter the number of squares per side (maximum 100):"
-  );
-  const newGridSize = parseInt(userInput);
-  if (!isNaN(newGridSize) && newGridSize > 0 && newGridSize <= 100) {
-    clearGrid();
-    container.style.width = `${newGridSize * 30}px`;
-    container.style.height = `${newGridSize * 30}px`;
-    for (let i = 0; i < newGridSize * newGridSize; i++) {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      container.appendChild(square);
-    }
-  } else {
-    alert("Invalid input. Please enter a valid number between 1 and 100.");
-  }
-}
-
-createGrid();
